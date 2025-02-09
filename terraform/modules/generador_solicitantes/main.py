52ef2440a84e8efb31cffe2f5dd3dad80fb16e3c
Claudiarg13 <cromerog13@gmail.com>
2025-02-09 19:10:46 +0100

    Update main.py

--- terraform/modules/generador_solicitantes/main.py
+++ terraform/modules/generador_solicitantes/main.py
@@ -11,8 +11,8 @@ from geopy.geocoders import Nominatim
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-PROJECT_ID = "endless-office-447815-d7"
-TOPIC_NAME = "solicitantes-topic"
+PROJECT_ID = os.getenv("PROJECT_ID")
+TOPIC_NAME = os.getenv("TOPIC_NAME_AYUDANTES")
 
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
